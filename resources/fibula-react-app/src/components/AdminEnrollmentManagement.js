cd708014bc9a994f1bfd18d7143ed7ad5f77ddd0
BJ Klingenberg <bklingen@calpoly.edu>
2025-02-04 12:28:11 -0800

    update backend link.

--- resources/fibula-react-app/src/components/AdminEnrollmentManagement.js
+++ resources/fibula-react-app/src/components/AdminEnrollmentManagement.js
@@ -1,128 +1,198 @@
-import { Button, IconButton, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import React, { useState, useEffect } from 'react';
-import { useParams, useSearchParams } from 'react-router-dom';
+import {
+  Button,
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import React, { useState, useEffect } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
 
 function AdminEnrollmentManagement() {
-    const [category, setCategory] = useState('Pending');
-    const [data, setData] = useState([]);
-    const [searchParams, setSearchParams] = useSearchParams();
+  const [category, setCategory] = useState("Pending");
+  const [data, setData] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
 
-    const email = searchParams.get('email');
+  const email = searchParams.get("email");
 
-    const handleApprove = async (row) => {
-      console.log('Approving enrollment request for ' + row.email);
-      console.log(row)
+  const handleApprove = async (row) => {
+    console.log("Approving enrollment request for " + row.email);
+    console.log(row);
 
-      await fetch(`https://vgyc6fujod.execute-api.us-east-1.amazonaws.com/prod/enroll/${row.requestid}`, {
+    await fetch(
+      `https://almpulq5w3.execute-api.us-east-2.amazonaws.com/prod/enroll/${row.requestid}`,
+      {
         method: "PUT",
         body: JSON.stringify({
-            response: "Approved"
+          response: "Approved",
         }),
-      })
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-        .catch((error) => console.error(error));
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
 
-      fetchData()
-        .catch((error) => console.error(error));
-    }
+    fetchData().catch((error) => console.error(error));
+  };
 
-    const handleDeny = async (row) => {
-      console.log('Denying enrollment request for ' + row.email);
-      console.log(row.requestid)
-      await fetch(`https://vgyc6fujod.execute-api.us-east-1.amazonaws.com/prod/enroll/${row.requestid}`, {
+  const handleDeny = async (row) => {
+    console.log("Denying enrollment request for " + row.email);
+    console.log(row.requestid);
+    await fetch(
+      `https://almpulq5w3.execute-api.us-east-2.amazonaws.com/prod/enroll/${row.requestid}`,
+      {
         method: "PUT",
         body: JSON.stringify({
-            response: "Denied"
+          response: "Denied",
         }),
-      })
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-        .catch((error) => console.error(error));
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
 
-      fetchData()
-        .catch((error) => console.error(error));
-    }
+    fetchData().catch((error) => console.error(error));
+  };
 
-    const ExpandableTableRow = ({ rowEmail, children, expandComponent, ...otherProps }) => {
-        const [isExpanded, setIsExpanded] = React.useState(false);
-      
-        return (
-          <>
-            <TableRow {...otherProps}>
-              <TableCell padding="checkbox">
-                <IconButton onClick={() => setIsExpanded(!isExpanded)}>
-                  {isExpanded || email === rowEmail ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                </IconButton>
-              </TableCell>
-              {children}
-            </TableRow>
-            {isExpanded && (
-              <TableRow>
-                <TableCell padding="checkbox" />
-                {expandComponent}
-              </TableRow>
-            )}
-          </>
-        );
-    };
-
-    const fetchData = async () => {
-        await fetch("https://vgyc6fujod.execute-api.us-east-1.amazonaws.com/prod/enroll")
-          .then((response) => response.json())
-          .then((data) => setData(data))
-    }
-
-    useEffect(() => {
-        fetchData()
-            .catch((error) => console.error(error));
-    }, []);
+  const ExpandableTableRow = ({
+    rowEmail,
+    children,
+    expandComponent,
+    ...otherProps
+  }) => {
+    const [isExpanded, setIsExpanded] = React.useState(false);
 
     return (
-        <div>
-            <center>
-                <h1>Admin Enrollment Management</h1>
-                <div>
-                    <Button variant={category === 'Pending' ? 'contained' : 'text'} color="primary" onClick={() => setCategory('Pending')}>Pending</Button>
-                    <Button variant={category === 'Approved' ? 'contained' : 'text'}  color="primary" onClick={() => setCategory('Approved')}>Approved</Button>
-                    <Button variant={category === 'Denied' ? 'contained' : 'text'}  color="secondary" onClick={() => setCategory('Denied')}>Denied</Button>
-                </div>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                    <TableRow>
-                        <TableCell padding="checkbox" />
-                        <TableCell align="right">Name</TableCell>
-                        <TableCell align="right">Email</TableCell>
-                        <TableCell align="right">Role</TableCell>
-                        <TableCell align="right">Status</TableCell>
-                        <TableCell align="right">Date Created</TableCell>
-                        {category !== 'Approved' ? <TableCell align="right">Approve</TableCell> : null}
-                        {category !== 'Denied' ? <TableCell align="right">Deny</TableCell> : null}
-                    </TableRow>
-                    </TableHead>
-                    <TableBody>
-                    {data.filter((request) => (request.enrollmentstatus === category)).map((row) => (
-                        <ExpandableTableRow
-                        email={row.email}
-                        key={row.requestid}
-                        expandComponent={<TableCell colSpan="5">{row.message}</TableCell>}
-                        >
-                            <TableCell align="right">{row.lastName}, {row.firstName}</TableCell>
-                            <TableCell align="right">{row.email}</TableCell>
-                            <TableCell align="right">{row.role}</TableCell>
-                            <TableCell align="right">{row.enrollmentstatus}</TableCell>
-                            <TableCell align="right">{row.dateCreated}</TableCell>
-                            {category !== 'Approved' ? <TableCell align="right"><Button variant="contained" color="primary" onClick={() => handleApprove(row)}>Approve</Button></TableCell> : null}
-                            {category !== 'Denied' ? <TableCell align="right"><Button variant="contained" color="secondary" onClick={() => handleDeny(row)}>Deny</Button></TableCell> : null}
-                        </ExpandableTableRow>
-                    ))}
-                    </TableBody>
-                </Table>
-            </center>
-        </div>
+      <>
+        <TableRow {...otherProps}>
+          <TableCell padding="checkbox">
+            <IconButton onClick={() => setIsExpanded(!isExpanded)}>
+              {isExpanded || email === rowEmail ? (
+                <KeyboardArrowUpIcon />
+              ) : (
+                <KeyboardArrowDownIcon />
+              )}
+            </IconButton>
+          </TableCell>
+          {children}
+        </TableRow>
+        {isExpanded && (
+          <TableRow>
+            <TableCell padding="checkbox" />
+            {expandComponent}
+          </TableRow>
+        )}
+      </>
     );
+  };
+
+  const fetchData = async () => {
+    await fetch(
+      "https://almpulq5w3.execute-api.us-east-2.amazonaws.com/prod/enroll"
+    )
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  };
+
+  useEffect(() => {
+    fetchData().catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      <center>
+        <h1>Admin Enrollment Management</h1>
+        <div>
+          <Button
+            variant={category === "Pending" ? "contained" : "text"}
+            color="primary"
+            onClick={() => setCategory("Pending")}
+          >
+            Pending
+          </Button>
+          <Button
+            variant={category === "Approved" ? "contained" : "text"}
+            color="primary"
+            onClick={() => setCategory("Approved")}
+          >
+            Approved
+          </Button>
+          <Button
+            variant={category === "Denied" ? "contained" : "text"}
+            color="secondary"
+            onClick={() => setCategory("Denied")}
+          >
+            Denied
+          </Button>
+        </div>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell padding="checkbox" />
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Email</TableCell>
+              <TableCell align="right">Role</TableCell>
+              <TableCell align="right">Status</TableCell>
+              <TableCell align="right">Date Created</TableCell>
+              {category !== "Approved" ? (
+                <TableCell align="right">Approve</TableCell>
+              ) : null}
+              {category !== "Denied" ? (
+                <TableCell align="right">Deny</TableCell>
+              ) : null}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data
+              .filter((request) => request.enrollmentstatus === category)
+              .map((row) => (
+                <ExpandableTableRow
+                  email={row.email}
+                  key={row.requestid}
+                  expandComponent={
+                    <TableCell colSpan="5">{row.message}</TableCell>
+                  }
+                >
+                  <TableCell align="right">
+                    {row.lastName}, {row.firstName}
+                  </TableCell>
+                  <TableCell align="right">{row.email}</TableCell>
+                  <TableCell align="right">{row.role}</TableCell>
+                  <TableCell align="right">{row.enrollmentstatus}</TableCell>
+                  <TableCell align="right">{row.dateCreated}</TableCell>
+                  {category !== "Approved" ? (
+                    <TableCell align="right">
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => handleApprove(row)}
+                      >
+                        Approve
+                      </Button>
+                    </TableCell>
+                  ) : null}
+                  {category !== "Denied" ? (
+                    <TableCell align="right">
+                      <Button
+                        variant="contained"
+                        color="secondary"
+                        onClick={() => handleDeny(row)}
+                      >
+                        Deny
+                      </Button>
+                    </TableCell>
+                  ) : null}
+                </ExpandableTableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </center>
+    </div>
+  );
 }
 
 export default AdminEnrollmentManagement;
--- resources/fibula-react-app/src/components/NewUserEnrollmentForm.js
+++ resources/fibula-react-app/src/components/NewUserEnrollmentForm.js
@@ -1,46 +1,51 @@
-import React, { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import { FormControl, FormLabel } from '@mui/material';
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { FormControl, FormLabel } from "@mui/material";
 
 function ContactForm() {
   const [submitted, setSubmitted] = useState(false);
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [organization, setOrganization] = useState('');
-  const [message, setMessage] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [organization, setOrganization] = useState("");
+  const [message, setMessage] = useState("");
 
   const handleSubmit = async (event) => {
     setSubmitted(true);
     event.preventDefault();
-    console.log(`First Name: ${firstName}\nLast Name: ${lastName}\nEmail: ${email}\nOrganization: ${organization}\nMessage: ${message}`);
+    console.log(
+      `First Name: ${firstName}\nLast Name: ${lastName}\nEmail: ${email}\nOrganization: ${organization}\nMessage: ${message}`
+    );
     // You can replace the console.log with your desired submit logic
-    await fetch("https://vgyc6fujod.execute-api.us-east-1.amazonaws.com/prod/enroll", {
+    await fetch(
+      "https://almpulq5w3.execute-api.us-east-2.amazonaws.com/prod/enroll",
+      {
         method: "PUT",
         body: JSON.stringify({
-            first_name: firstName,
-            last_name: lastName,
-            email: email,
-            role: 1,
-            organization: organization,
-            message: message,
+          first_name: firstName,
+          last_name: lastName,
+          email: email,
+          role: 1,
+          organization: organization,
+          message: message,
         }),
         headers: {
-            "Content-Type": "application/json"
-        }
-    })
+          "Content-Type": "application/json",
+        },
+      }
+    )
       .then((response) => response.json())
       .then((data) => console.log(data))
       .catch((error) => console.error(error));
 
     setTimeout(() => {
       setSubmitted(false);
-      setFirstName('');
-      setLastName('');
-      setEmail('');
-      setOrganization('');
-      setMessage('');
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+      setOrganization("");
+      setMessage("");
     }, 1000);
   };
 
@@ -49,18 +54,43 @@ function ContactForm() {
       <center>
         <h1>New User Enrollment Form</h1>
         <FormControl>
-            {submitted && <p>Submitted!</p>}
-            <FormLabel>First Name</FormLabel>
-            <TextField type="text" value={firstName} onChange={(firstName) => setFirstName(firstName.target.value)}></TextField>
-            <FormLabel>Last Name</FormLabel>
-            <TextField type="text" value={lastName} onChange={(lastName) => setLastName(lastName.target.value)}></TextField>
-            <FormLabel>Email</FormLabel>
-            <TextField type="email" value={email} onChange={(email) => setEmail(email.target.value)}></TextField>
-            <FormLabel>Organization</FormLabel>
-            <TextField type="text" value={organization} onChange={(organization) => setOrganization(organization.target.value)}></TextField>
-            <FormLabel>Message</FormLabel>
-            <TextField id="outlined-multiline-flexible" multiline maxRows={4} value={message} type="text" onChange={(message) => setMessage(message.target.value)}></TextField>
-            <Button onClick={handleSubmit}>Submit</Button>
+          {submitted && <p>Submitted!</p>}
+          <FormLabel>First Name</FormLabel>
+          <TextField
+            type="text"
+            value={firstName}
+            onChange={(firstName) => setFirstName(firstName.target.value)}
+          ></TextField>
+          <FormLabel>Last Name</FormLabel>
+          <TextField
+            type="text"
+            value={lastName}
+            onChange={(lastName) => setLastName(lastName.target.value)}
+          ></TextField>
+          <FormLabel>Email</FormLabel>
+          <TextField
+            type="email"
+            value={email}
+            onChange={(email) => setEmail(email.target.value)}
+          ></TextField>
+          <FormLabel>Organization</FormLabel>
+          <TextField
+            type="text"
+            value={organization}
+            onChange={(organization) =>
+              setOrganization(organization.target.value)
+            }
+          ></TextField>
+          <FormLabel>Message</FormLabel>
+          <TextField
+            id="outlined-multiline-flexible"
+            multiline
+            maxRows={4}
+            value={message}
+            type="text"
+            onChange={(message) => setMessage(message.target.value)}
+          ></TextField>
+          <Button onClick={handleSubmit}>Submit</Button>
         </FormControl>
       </center>
     </div>
